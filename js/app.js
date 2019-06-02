744e23c719a1d5af77124f8f18cc845f6ea77615
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-02 18:36:44 +0100

    Added logic for accepting voice input and displaying the input

--- js/app.js
+++ js/app.js
@@ -14,4 +14,31 @@
 
 		document.getElementById("voice-player-component").speak();
 	});
+
+	/* Voice Recognition */
+	let voiceRecognitionElement = document.getElementById("voice-recognition-element");
+	let voiceRecognitionStart = document.getElementById("voice-recognition-start");
+	let voiceRecognitionStop = document.getElementById("voice-recognition-stop");
+	let voiceRecognitionOutput = document.getElementById("voice-recognition-output");
+	let voiceRecognitionText = "";
+
+	voiceRecognitionStart.addEventListener("click", function(event) {
+		event.preventDefault();
+
+		voiceRecognitionElement.start();
+	});
+
+	voiceRecognitionElement.addEventListener("result", function(event) {
+		voiceRecognitionText = event.detail.result;
+	});
+
+	voiceRecognitionStop.addEventListener("click", function(event) {
+		event.preventDefault();
+
+		voiceRecognitionElement.stop();
+	});
+
+	voiceRecognitionElement.addEventListener("end", function(event) { 
+		voiceRecognitionOutput.textContent = voiceRecognitionText;
+	});
 }());
