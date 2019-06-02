5bc8240a4feb37b81c54abb6ac8f05920c02e1bc
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-02 17:12:45 +0100

    Added click event listener to the voice speech button
    The activates the voice speech component when the button is clicked

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 (function() {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
+
+	/* Voice speech */
+	let voicePlayerSubmit = document.getElementById("voice-player-submit");
+	voicePlayerSubmit.addEventListener("click", function(event) {
+		event.preventDefault();
+
+		document.getElementById("voice-player-component").speak();
+	});
 }());
