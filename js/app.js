10e7a4b0192db9e33b55121b08231b4f09a8ff54
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-02 17:16:42 +0100

    Added event listener to the voice speech text input field
    The voice now speaks the text that was entered in the input field

--- js/app.js
+++ js/app.js
@@ -3,6 +3,12 @@
 
 	/* Voice speech */
 	let voicePlayerSubmit = document.getElementById("voice-player-submit");
+	let voicePlayerText = document.getElementById("voice-player-text");
+
+	voicePlayerText.addEventListener("input", function(event) {
+		document.getElementById("voice-player-component").setAttribute("text", event.target.value);
+	});
+
 	voicePlayerSubmit.addEventListener("click", function(event) {
 		event.preventDefault();
 
